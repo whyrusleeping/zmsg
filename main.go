cabbc8f85d7a7d6013d36f4025de07207bd3c755
Jeromy <why@ipfs.io>
2016-11-11 17:08:48 -0800

    read auth from config file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/base64"
 	"encoding/hex"
@@ -8,14 +9,58 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
 	cli "github.com/urfave/cli"
 )
 
-var username = "username"
-var password = "foobarpasswordcoolstuff"
+var username string
+var password string
+
+func init() {
+	u, p, err := readAuthCreds()
+	if err != nil {
+		fmt.Println("Error reading zcash config: ", err)
+	}
+
+	username = u
+	password = p
+}
+
+func readAuthCreds() (string, string, error) {
+	homedir := os.Getenv("HOME")
+	confpath := filepath.Join(homedir, ".zcash/zcash.conf")
+	fi, err := os.Open(confpath)
+	if err != nil {
+		return "", "", err
+	}
+	defer fi.Close()
+
+	var user string
+	var pass string
+	scan := bufio.NewScanner(fi)
+	for scan.Scan() {
+		parts := strings.SplitN(scan.Text(), "=", 2)
+		if len(parts) < 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+
+		switch key {
+		case "rpcuser":
+			user = val
+		case "rpcpassword":
+			pass = val
+		}
+	}
+
+	return user, pass, nil
+}
 
 type Message struct {
 	From    string
